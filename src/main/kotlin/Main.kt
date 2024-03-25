4dd2faa12e2d5142bd499e27f29f1f25b4021229
stephanie-niva <stephanienivaa@example.com>
2024-03-25 08:43:16 +0300

    add changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 fun main() {
-    var result=fun edit("Barnie bakes brown bagels and buns)
+    var result=fun edit("Barnie bakes brown bagels and buns")
+    var sum =fun numbers()
 }
 
 
@@ -7,6 +8,14 @@ fun edit(x:String){
     var result = x.slice(0, 7, 13, 19)
     println(result)
 }
-fun number(b:arrayOf<Int>):Int{
+fun number(b:Array<Int>):Int{
+    var sum= b.sum()
+    println(sum)
+    var count= b.count()
+    println(count)
+    var average= b.average()
+    println(average)
+
+
 
 }
