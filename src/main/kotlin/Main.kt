307defb3d9ae379f1b89a49568ba44e70cd3dd9c
stephanie-niva <stephanienivaa@example.com>
2024-03-25 08:21:57 +0300

    add file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var result=fun edit("Barnie bakes brown bagels and buns)
+}
+
+
+fun edit(x:String){
+    var result = x.slice(0, 7, 13, 19)
+    println(result)
+}
+fun number(b:arrayOf<Int>):Int{
+
+}
